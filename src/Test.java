e0157f63fe3d1217afcbb7be0d88b6ec26754902
César Arturo González Pérez <arthurgonzalezp@gmail.com>
2016-04-12 12:12:19 -0500

    MPPNode Class Created
    Created class MPPNode in order to use it in the graph

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,16 @@
+import mx.arturogonzalezp.mppdb.structures.MPPNode;
+
 
 public class Test {
 
 	public static void main(String[] args) {
-		
+		MPPNode<String> nodoPadre = new MPPNode<String>("Padre", 310);
+		nodoPadre.setLevel(0);
+		nodoPadre.addChildNode(new MPPNode<String>("Hijo"));
+		nodoPadre.getChildNodeAt(0).addChildNode(new MPPNode<String>("Nieto", 100));
+		System.out.println(nodoPadre);
+		System.out.println(nodoPadre.getChildNodeAt(0));
+		System.out.println(nodoPadre.getChildNodeAt(0).getChildNodeAt(0));
 	}
 
 }
---
+++ src/mx/arturogonzalezp/mppdb/structures/MPPNode.java
@@ -0,0 +1,78 @@
+package mx.arturogonzalezp.mppdb.structures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MPPNode <T extends Comparable<T>> implements Comparable<MPPNode<T>>{
+	private List<MPPNode<T>> childNodes;
+	private T item;
+	private int storageKey;
+	private int level;
+	public MPPNode(){
+		this.setItem(null);
+		this.setChildNodes(new ArrayList<MPPNode<T>>());
+		this.setStorageKey(0);
+		this.setLevel(-1);
+	}
+	public MPPNode(T nodeItem){
+		this();
+		this.setItem(nodeItem);
+	}
+	public MPPNode(T nodeItem,int storageKey){
+		this(nodeItem);
+		this.setStorageKey(storageKey);
+	}
+	public List<MPPNode<T>> getChildNodes() {
+		return childNodes;
+	}
+	public void setChildNodes(List<MPPNode<T>> childNodes) {
+		this.childNodes = childNodes;
+	}
+	public T getItem() {
+		return item;
+	}
+	public void setItem(T item) {
+		this.item = item;
+	}
+	public int getStorageKey() {
+		return storageKey;
+	}
+	public void setStorageKey(int storageKey) {
+		this.storageKey = storageKey;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public boolean addChildNode(MPPNode<T> childNode){
+		if(this.getLevel() > -1){
+			childNode.setLevel(this.getLevel()+1);
+			this.getChildNodes().add(childNode);
+			return true;
+		}
+		return false;
+	}
+	public MPPNode<T> getChildNodeAt(int index){
+		return this.getChildNodes().get(index);
+	}
+	public int getChildNodesCount(){
+		return this.getChildNodes().size();
+	}
+	public void removeChildNodeAt(int index){
+		this.getChildNodes().remove(index);
+	}
+	public String toString(){
+		return "Storage Key: " + this.getStorageKey() + "\nLevel: " + this.getLevel() + "\nChild Nodes Count: " + this.getChildNodesCount() + "\nItem:\n " + this.getItem().toString() + "\n";
+	}
+	@Override
+	public int compareTo(MPPNode<T> node) {
+		if(this.getItem().compareTo(node.getItem()) < 0){
+			return -1;
+		}else if(this.getItem().compareTo(node.getItem()) > 0){
+			return 1;
+		}
+		return 0;
+	}
+}
